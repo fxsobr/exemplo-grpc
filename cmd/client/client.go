56461cc4085b7cb0c9d00788224ac3f0f925e38b
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-06-17 15:08:41 -0300

    implementado  client para serverstream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/fxsobr/exemplo-grpc/pb/pb"
@@ -18,7 +19,8 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	// AddUser(client)
+	AddUserVerbose(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -35,5 +37,30 @@ func AddUser(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Marcelo Ceolin",
+		Email: "m@.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not receive the message: %v", err)
+		}
+		fmt.Println("Status:", stream.Status, stream.GetUser())
+	}
 
 }
