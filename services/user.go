5ff5cad41995c34ef3984b8f00e27ef4bba619cb
Marcelo Ceolin <mceolin@unidavi.edu.br>
2021-06-17 14:23:34 -0300

    Criado Service de Usuario

---
+++ services/user.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/fxsobr/exemplo-grpc/pb/pb"
+)
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error) {
+
+	// Insert - Database
+	fmt.Println(req.Name)
+
+	return &pb.User{
+		Id:    "123",
+		Name:  req.GetName(),
+		Email: req.Email,
+	}, nil
+}
